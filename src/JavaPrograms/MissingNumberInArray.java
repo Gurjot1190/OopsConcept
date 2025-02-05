a2c1b0eb5a4a05af92cae3379b9c88bb3c43f89e
GurjotAutomation <gurjotshergill11@gmail.com>
2025-02-05 13:27:47 +1100

    new testing

--- src/JavaPrograms/MissingNumberInArray.java
+++ src/JavaPrograms/MissingNumberInArray.java
@@ -22,7 +22,7 @@ public class MissingNumberInArray {
         System.out.println("Sum of range is = "+sum2);
         System.out.println("Missing Number in Array is = "+(sum2-sum1));
 
-        //Comment for testing
+        //Comment for testingnb
 
 
         
